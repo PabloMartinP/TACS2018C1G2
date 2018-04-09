1f4f96420a8dc1a3e4daeea1782320a343a2da05
ezmacias <>
2018-04-08 23:04:18 -0300

    Agregamos endpoint para consultar usuario.

--- snake/src/main/java/com/utn/tacs/grupo2/snake/controller/UsuarioRestController.java
+++ snake/src/main/java/com/utn/tacs/grupo2/snake/controller/UsuarioRestController.java
@@ -2,8 +2,11 @@ package com.utn.tacs.grupo2.snake.controller;
 
 import com.utn.tacs.grupo2.snake.domain.Billetera;
 import com.utn.tacs.grupo2.snake.domain.Moneda;
+import com.utn.tacs.grupo2.snake.domain.TipoDeTransaccion;
+import com.utn.tacs.grupo2.snake.domain.Transaccion;
 import com.utn.tacs.grupo2.snake.domain.Usuario;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,13 +16,26 @@ public class UsuarioRestController {
 
     @PostMapping("/usuarios")
     public Usuario guardar(@RequestBody Usuario usuario) {
-        Usuario respuesta = new Usuario("admin", "admin", new ArrayList<>(), null, new ArrayList<>());
-        return respuesta;
+        Usuario usuarioMock = new Usuario("user", "pass", new ArrayList<>(), null, new ArrayList<>());
+        return usuarioMock;
     }
 
+    @GetMapping("/usuarios/{id}")
+    public Usuario obtener(@RequestParam Long id){
+        List<Billetera> portfolio = new ArrayList<>();
+        Moneda bitcoin = new Moneda("Bitcoin", new Double("573.137"), new Double("1.0"));
+        portfolio.add(new Billetera(bitcoin, new Double("0.01")));
+        
+        List<Transaccion> transacciones = new ArrayList<>();
+        transacciones.add(new Transaccion(bitcoin, new Date(), 33.0, bitcoin.getCotizacionBitcoin(), TipoDeTransaccion.COMPRA));
+        
+        Usuario usuario = new Usuario("user", "pass", portfolio, new Date(), transacciones);
+        return usuario;
+    }
+    
     @GetMapping("/usuarios/{id}/portfolio")
     public List<Billetera> obtenerPortfolio(@RequestParam Long id) {
-        ArrayList<Billetera> portfolio = new ArrayList<>();
+        List<Billetera> portfolio = new ArrayList<>();
 
         Moneda bitcoin = new Moneda("Bitcoin", new Double("573.137"), new Double("1.0"));
         portfolio.add(new Billetera(bitcoin, new Double("0.01")));
@@ -36,5 +52,5 @@ public class UsuarioRestController {
         
         return billetera;
     }
-
+    
 }
