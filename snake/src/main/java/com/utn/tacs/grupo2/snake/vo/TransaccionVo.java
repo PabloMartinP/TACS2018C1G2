a7c56919b0118884ea867b824ce37e1690253093
Laureano clausi <laureano.clausi@gmail.com>
2018-04-11 17:42:23 -0300

    arreglo monedaVo

--- snake/src/main/java/com/utn/tacs/grupo2/snake/vo/TransaccionVo.java
+++ snake/src/main/java/com/utn/tacs/grupo2/snake/vo/TransaccionVo.java
@@ -1,6 +1,5 @@
 package com.utn.tacs.grupo2.snake.vo;
 
-import com.utn.tacs.grupo2.snake.domain.Moneda;
 import com.utn.tacs.grupo2.snake.domain.Transaccion;
 import java.util.Date;
 import lombok.Data;
@@ -9,14 +8,14 @@ import org.springframework.hateoas.ResourceSupport;
 @Data
 public class TransaccionVo extends ResourceSupport {
 
-    private Moneda moneda;
+    private MonedaVo moneda;
     private Date fechaDeCreacion;
     private Double cantidad;
     private Double cotizacion;
     private String tipo;
 
     public TransaccionVo(Transaccion transaccion) {
-        this.moneda = transaccion.getMoneda();
+        this.moneda = new MonedaVo(transaccion.getMoneda());
         this.fechaDeCreacion = transaccion.getFechaDeCreacion();
         this.cantidad = transaccion.getCantidad();
         this.cotizacion = transaccion.getCotizacion();
