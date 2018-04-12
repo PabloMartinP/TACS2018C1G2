9ad4f1ff1c82b4efa335088abb2b366c68d99c22
Laureano clausi <laureano.clausi@gmail.com>
2018-04-12 12:01:46 -0300

    agergo configuracion para que los links sean objetos y no un array

--- snake/src/main/java/com/utn/tacs/grupo2/snake/SnakeApplication.java
+++ snake/src/main/java/com/utn/tacs/grupo2/snake/SnakeApplication.java
@@ -2,11 +2,14 @@ package com.utn.tacs.grupo2.snake;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
+import org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType;
 
 @SpringBootApplication
+@EnableHypermediaSupport(type = HypermediaType.HAL)
 public class SnakeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SnakeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SnakeApplication.class, args);
+    }
 }
