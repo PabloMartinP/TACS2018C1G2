795951c5bdaaf192c67a7ac62d0c63d9a74b1ae4
ezmacias <pc@DESKTOP-KNPSRME.home>
2018-04-28 16:09:44 -0300

    Agregamos Thymeleaf para servir el html.

--- snake/src/main/java/com/utn/tacs/grupo2/snake/controller/HomeController.java
+++ snake/src/main/java/com/utn/tacs/grupo2/snake/controller/HomeController.java
@@ -4,11 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String home() {
         return "home";
     }
