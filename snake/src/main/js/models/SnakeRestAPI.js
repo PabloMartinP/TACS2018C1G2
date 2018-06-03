bf1d36e8f287b56b4574ab580a5f0a5a63f7f1ac
Laureano Clausi <laureano.clausi@gmail.com>
2018-06-03 12:04:32 -0300

    arreglo fetchs y encode de las passwords

--- snake/src/main/js/models/SnakeRestAPI.js
+++ snake/src/main/js/models/SnakeRestAPI.js
@@ -6,6 +6,7 @@ const SnakeRestAPI = {
     registrarTransaccion(unaTransaccion) {
         return fetch('/api/transacciones', {
             method: 'POST',
+            credentials: "same-origin",
             body: JSON.stringify({monedaNombre: unaTransaccion.criptomoneda, cantidad: unaTransaccion.cantidad, tipo: unaTransaccion.nombre.toUpperCase()}),
             headers: {'Content-Type': 'application/json'}
         })
@@ -15,11 +16,11 @@ const SnakeRestAPI = {
         // TODO: Para aliviar este quilombo sería mejor que cuando pida las monedasDisponibles, además del nombre ya me de la cotización
         // FIXME: Las monedas que obtengo de acá NO son las mismas que tiene un usuario, por eso no puedo saber su cotización actual
         let cotizaciones = new Map();
-        return fetch('/api/monedas')
+        return fetch('/api/monedas', {credentials: "same-origin"})
             .then(respuesta => respuesta.json())
             .then(monedasEnJson => monedasEnJson.reduce((request, monedaEnJson) => {
                 return request
-                    .then(() => fetch(monedaEnJson._links.cotizacion.href))
+                    .then(() => fetch(monedaEnJson._links.cotizacion.href), {credentials: "same-origin"})
                     .then(respuesta => respuesta.json())
                     .then(cotizacionActual => cotizaciones.set(monedaEnJson.nombre, cotizacionActual.cotizacionDolar));
             }, Promise.resolve())
@@ -32,14 +33,14 @@ const SnakeRestAPI = {
             .then(respuesta => respuesta.json())
             .then(usuarioEnJson => {
                 usuario = Usuario.crear(usuarioEnJson.username, this);
-                return fetch(usuarioEnJson._links.portfolio.href);
+                return fetch(usuarioEnJson._links.portfolio.href, {credentials: "same-origin"});
             })
             .then(respuesta => respuesta.json())
             .then(portfolioEnJson => {
                 portfolioEnJson.forEach(monedaEnJson => {
                     const criptomoneda = monedaEnJson.moneda.nombre;
                     usuario.agregarCriptomoneda(criptomoneda, monedaEnJson.cantidad);
-                    fetch(monedaEnJson._links.transacciones.href)
+                    fetch(monedaEnJson._links.transacciones.href, {credentials: "same-origin"})
                         .then(respuesta => respuesta.json())
                         .then(transaccionesEnJson => transaccionesEnJson.forEach(transaccionEnJson => {
                             const tipoDeTransaccion = transaccionEnJson.tipo === 'COMPRA' ? Compra : Venta;
