767ba87f8dd6477be983567a583d343112df91d2
Nicole Tobal <nickytobal@gmail.com>
2018-06-03 17:12:41 -0300

    Pantalla de Admin (parte visual)

--- snake/src/main/js/app.js
+++ snake/src/main/js/app.js
@@ -2,18 +2,26 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Portfolio from './components/Portfolio/Portfolio.jsx';
+import Administracion from './components/Administracion/Administracion.jsx';
 import './App.css';
 import Header from './components/Header/Header.jsx';
 import Login from './components/Login/Login.jsx';
 
 class App extends React.Component {
     getComponent() {
+        console.log(this.props.reactComponent);
         switch (this.props.reactComponent) {
             case 'portfolio':
+                // return (
+                //     <div className="App">
+                //         <Header userName={'Chester'}/>
+                //         <Portfolio usuarioId={1}/>
+                //     </div>
+                // );
                 return (
                     <div className="App">
                         <Header userName={'Chester'}/>
-                        <Portfolio usuarioId={1}/>
+                        <Administracion/>
                     </div>
                 );
             default:
---
+++ snake/src/main/js/components/Administracion/Administracion.jsx
@@ -0,0 +1,241 @@
+import React, {Component} from 'react';
+import {
+    Table,
+    TableBody,
+    TableHeader,
+    TableHeaderColumn,
+    TableRow,
+    TableRowColumn,
+} from 'material-ui/Table';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import Checkbox from 'material-ui/Checkbox';
+import '../Portfolio/Portfolio.css';
+import {SnakeRestAPI} from '../../models/SnakeRestAPI';
+const moment = require('moment');
+
+export default class Administracion extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            usuario: null,
+            cantTransacciones: {
+                hoy: {
+                    name: 'En el día de hoy',
+                    value: 100
+                },
+                ult3Dias: {
+                    name: 'En los últimos 3 días',
+                    value: 100
+                },
+                ultSemana: {
+                    name: 'En la última semana',
+                    value: 100
+                },
+                ultMes: {
+                    name: 'En el último mes',
+                    value: 100
+                },
+                todas: {
+                    name: 'Desde el inicio de los tiempos',
+                    value: 100
+                }
+            },
+            infoUsuarios: {
+                usuario: {
+                    name: 'Usuario',
+                    value: 'Chester'
+                },
+                cantCripto: {
+                    name: 'Cantidad de criptomonedas',
+                    value: '2 (bitcoin, etherum)'
+                },
+                cantTxs: {
+                    name: 'Cantidad de transacciones',
+                    value: 100
+                },
+                ultimoAcceso: {
+                    name: 'Último acceso',
+                    value: moment(new Date()).format('DD/MM/YYYY')
+                }
+            },
+            showSecondFilter: false,
+            userFilter1: '',
+            userFilter2: '',
+            errors: {
+                error1: {
+                    text: 'Debe completar este campo',
+                    displayError: false
+                },
+                error2: {
+                    text: 'Debe completar ambos campos',
+                    displayError: false
+                }
+            },
+            resultadoComparacion: ''
+        };
+    }
+
+    // actualizarInfoPortfolio() {
+    //     SnakeRestAPI.obtenerUsuario()
+    //         .then(usuario => this.setState({ usuario }));
+
+    //     SnakeRestAPI.obtenerCotizador()
+    //         .then(cotizador => this.setState({ cotizador }));
+    // }
+
+    displaySecondFilter() {
+        if (this.state.showSecondFilter) {
+            return (
+                <div className="row mb-20">
+                    <div className="input col-md-3">
+                        <br/>
+                        <TextField
+                            hintText="Usuario"
+                            style={{width: "90%", marginLeft: "10px"}}
+                            onChange={(e) => {
+                                const { errors } = this.state;
+                                errors.error2.displayError = false;
+                                this.setState({userFilter2: e.target.value});
+                            }}
+                            errorText={
+                                this.state.errors.error2.displayError ?
+                                this.state.errors.error2.text :
+                                ''
+                            }
+                        />
+                    </div>
+                    <div>
+                        <br/>
+                        <RaisedButton primary={true} onClick={this.compareUsers.bind(this)}>
+                            COMPARAR
+                        </RaisedButton>
+                    </div>
+                    <div className="comparisson-result">
+                        <br />
+                        <p>{this.state.resultadoComparacion}</p>
+                    </div>
+                </div>
+            );
+        }
+        return '';
+    }
+
+    searchUsers() {
+        if (this.state.userFilter1 === '') {
+            const { errors } = this.state;
+            errors.error1.displayError = true;
+            this.setState({errors});
+            return;
+        }
+    }
+
+    compareUsers() {
+        if (this.state.userFilter1 === '' || this.state.userFilter2 === '') {
+            const { errors } = this.state;
+            errors.error2.displayError = true;
+            this.setState({errors});
+            return;
+        }
+        this.setState({
+            resultadoComparacion: this.state.userFilter1 + " tiene mayor capital que "
+                + this.state.userFilter2
+        });
+    }
+
+    componentDidMount() {
+        //this.actualizarInfoPortfolio();
+    }
+
+    render() {
+        return (
+            <div className="table-container admin">
+                <h5> Información sobre transacciones </h5>
+                <Table>
+                    <TableHeader displaySelectAll={false}>
+                        <TableRow>
+                            <TableHeaderColumn></TableHeaderColumn>
+                            <TableHeaderColumn>Cantidad de Transacciones</TableHeaderColumn>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody displayRowCheckbox={false}>
+                        {
+                            Object.values(this.state.cantTransacciones).map((cantTransaccion, index) => (
+                                <TableRow key={index}>
+                                    <TableRowColumn>{cantTransaccion.name}</TableRowColumn>
+                                    <TableRowColumn>{cantTransaccion.value}</TableRowColumn>
+                                </TableRow>
+                            ))
+                        }
+                    </TableBody>
+                </Table>
+                <h5> Información sobre usuarios </h5>
+                <div className="row mb-20">
+                    <div className="input col-md-3">
+                        <br/>
+                        <TextField
+                            hintText="Usuario"
+                            style={{width: "90%", marginLeft: "10px"}}
+                            onChange={(e) => {
+                                const { errors } = this.state;
+                                errors.error1.displayError = false;
+                                errors.error2.displayError = false;
+                                this.setState({userFilter1: e.target.value, errors})
+                            }}
+                            errorText={
+                                this.state.errors.error1.displayError ?
+                                this.state.errors.error1.text :
+                                ''
+                            }
+                        />
+                    </div>
+                    <div>
+                        <br/>
+                        <RaisedButton primary={true} onClick={this.searchUsers.bind(this)}>
+                            BUSCAR
+                        </RaisedButton>
+                    </div>
+                    <div className="col-md-7 checkbox-input">
+                        <br/>
+                        <div className="row">
+                            <div className="col-md-2">
+                                <Checkbox
+                                    checked={this.state.showSecondFilter}
+                                    onCheck={(event) => {
+                                        const { errors } = this.state;
+                                        errors.error2.displayError = false;
+                                        this.setState(
+                                            {
+                                                showSecondFilter: event.target.checked,
+                                                userFilter2: '',
+                                                errors,
+                                                resultadoComparacion: ''
+                                            })
+                                        }
+                                    }
+                                >
+                                </Checkbox>
+                            </div>
+                            <div className="col-md-7">
+                                <p>Comparar con otro usuario </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {this.displaySecondFilter()}
+                <Table>
+                    <TableBody displayRowCheckbox={false}>
+                        {
+                            Object.values(this.state.infoUsuarios).map((info, index) => (
+                                <TableRow key={index}>
+                                    <TableRowColumn>{info.name}</TableRowColumn>
+                                    <TableRowColumn>{info.value}</TableRowColumn>
+                                </TableRow>
+                            ))
+                        }
+                    </TableBody>
+                </Table>
+            </div>
+        )
+    }
+}
