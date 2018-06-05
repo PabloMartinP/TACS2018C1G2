59ef7eac647736d13ee5f09cd94e089acf23ddcc
Laureano clausi <laureano.clausi@gmail.com>
2018-06-05 14:55:09 -0300

    cambio la bd a una mysql

--- snake/src/main/java/com/utn/tacs/grupo2/snake/SnakeApplication.java
+++ snake/src/main/java/com/utn/tacs/grupo2/snake/SnakeApplication.java
@@ -1,6 +1,5 @@
 package com.utn.tacs.grupo2.snake;
 
-import com.utn.tacs.grupo2.snake.telegram.TelegramUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
@@ -13,8 +12,7 @@ public class SnakeApplication {
     public static void main(String[] args) {
         SpringApplication.run(SnakeApplication.class, args);
 
-        Boolean estaEnProduccion = true;
-        TelegramUtils.Start(estaEnProduccion);
-
+//        Boolean estaEnProduccion = true;
+//        TelegramUtils.Start(estaEnProduccion);
     }
 }
